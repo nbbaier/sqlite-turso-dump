8ba02b9d30d234982d56c8701c7bae3fd33fe0dc
Nico Baier <nico.baier@gmail.com>
2024-09-07 22:22:59 -0500

    syncing

--- database/dump.sql
+++ database/dump.sql
@@ -1,16 +1,30 @@
-PRAGMA foreign_keys=OFF;
+PRAGMA foreign_keys = OFF;
+
 BEGIN TRANSACTION;
-CREATE TABLE employees (
-id INTEGER PRIMARY KEY,
-name TEXT,
-address JSON
-);
-INSERT INTO employees VALUES(1,'John Doe',replace('{\n"streetAddress": "21 2nd Street",\n"city": "New York",\n"state": "NY",\n"postalCode": "10021"\n}','\n',char(10)));
-CREATE TABLE COMPANY(
-   ID INT PRIMARY KEY     NOT NULL,
-   NAME           TEXT    NOT NULL,
-   AGE            INT     NOT NULL,
-   ADDRESS        CHAR(50),
-   SALARY         REAL    CHECK(SALARY > 0)
-);
+
+CREATE TABLE
+   employees (id INTEGER PRIMARY KEY, name TEXT, address JSON);
+
+INSERT INTO
+   employees
+VALUES
+   (
+      1,
+      'John Doe',
+      replace(
+         '{\n"streetAddress": "21 2nd Street",\n"city": "New York",\n"state": "NY",\n"postalCode": "10021"\n}',
+         '\n',
+         char(10)
+      )
+   );
+
+CREATE TABLE
+   COMPANY (
+      ID INT PRIMARY KEY NOT NULL,
+      NAME TEXT NOT NULL,
+      AGE INT NOT NULL,
+      ADDRESS CHAR(50),
+      SALARY REAL CHECK (SALARY > 0)
+   );
+
 COMMIT;
--- index.ts
+++ index.ts
@@ -1,43 +1,43 @@
 import { createClient } from "@libsql/client";
 
 const sqlite = createClient({
-  url: "file:./database/new.db",
+	url: "file:./database/new.db",
 });
 
 const tables = (
-  await sqlite.execute(`select name from sqlite_schema`)
+	await sqlite.execute("select name from sqlite_schema")
 ).rows.map((row) => row.name);
 console.log(tables);
 
-let statements = [];
+const statements = [];
 
 for (const table of tables) {
-  const schema = await sqlite.execute({
-    sql: `SELECT name, sql FROM sqlite_schema WHERE name = '${table}'`,
-    args: [],
-  });
+	const schema = await sqlite.execute({
+		sql: `SELECT name, sql FROM sqlite_schema WHERE name = '${table}'`,
+		args: [],
+	});
 
-  const createStatement = schema.rows.map((row) =>
-    row.sql?.toString()
-  ) as string[];
+	const createStatement = schema.rows.map((row) =>
+		row.sql?.toString(),
+	) as string[];
 
-  const data = await sqlite.execute(`select * from ${table}`);
+	const data = await sqlite.execute(`select * from ${table}`);
 
-  const importStatements = data.rows.map((row) => {
-    const rowData = Object.entries(row).reduce(
-      (obj, [key, value]) => ({ ...obj, [key]: value }),
-      {}
-    );
-    return `insert into ${table} (${Object.keys(rowData).join(
-      ", "
-    )}) values (${Object.values(rowData).join(", ")})`;
-  });
+	const importStatements = data.rows.map((row) => {
+		const rowData = Object.entries(row).reduce(
+			(obj, [key, value]) => Object.assign(obj, { [key]: value }),
+			{},
+		);
+		return `insert into ${table} (${Object.keys(rowData).join(
+			", ",
+		)}) values (${Object.values(rowData).join(", ")})`;
+	});
 
-  statements.push(...createStatement, ...importStatements);
+	statements.push(...createStatement, ...importStatements);
 }
 
 const dumpTemplate = (statements: string[]) => {
-  return `BEGIN TRANSACTION;
+	return `BEGIN TRANSACTION;
 ${statements.join(";\n")}
 COMMIT;`;
 };
